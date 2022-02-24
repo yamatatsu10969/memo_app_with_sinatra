70116dde5235e998e8990bfc6cfcd73f6e750d4c
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-02-24 22:21:48 +0900

    localhost で起動できるように thin を追加 再起動せずに反映できるように sinatra-contrib を追加

--- Gemfile
+++ Gemfile
@@ -4,4 +4,6 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'thin'
 
---
+++ memo_app.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  # 'hello'
+  'good bye!!!!!!!'
+end
