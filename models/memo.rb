ee97daf0f3a7109f298c1ff0ee34b1cc490dcac3
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-02-26 16:49:14 +0900

    memo のモデルを追加

---
+++ models/memo.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+class Memo
+  attr_reader :id, :title, :description
+
+  def initialize(id, title, description)
+    @id = id
+    @title = title
+    @description = description
+  end
+
+  def self.json_create(object)
+    new(object['id'], object['title'], object['description'])
+  end
+
+  def as_json(*)
+    {
+      JSON.create_id => self.class.name,
+      'id' => id,
+      'title' => title,
+      'description' => description
+    }
+  end
+
+  def to_json(*)
+    as_json.to_json
+  end
+end
